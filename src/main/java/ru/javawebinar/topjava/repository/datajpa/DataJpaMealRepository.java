31bef1139efc2975b81a9407f759e6b4bb60d640
Alexandr <francuzby@gmail.com>
2021-11-09 14:05:54 +0300

    HW5 Add new functional for home work
    Add new functional for home work

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -10,37 +11,49 @@ import java.util.List;
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
-    private final CrudMealRepository crudRepository;
+    private final CrudMealRepository crudMealRepository;
     private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
-        this.crudRepository = crudRepository;
+    @Autowired
+    public DataJpaMealRepository(CrudMealRepository crudMealRepository, CrudUserRepository crudUserRepository) {
+        this.crudMealRepository = crudMealRepository;
         this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
         meal.setUser(crudUserRepository.getById(userId));
-        return crudRepository.save(meal);
+        if (meal.isNew()) {
+            crudMealRepository.save(meal);
+            return meal;
+        } else if (get(meal.id(), userId) == null) {
+            return null;
+        }
+        return crudMealRepository.saveAndFlush(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudMealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return crudRepository.getById(userId);
+        Meal meal = crudMealRepository.findById(id).orElse(null);
+        if (meal != null && meal.getUser().getId() == userId) {
+            return meal;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudMealRepository.findAllById(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudMealRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
@@ -11,34 +12,35 @@ import java.util.List;
 public class DataJpaUserRepository implements UserRepository {
     private static final Sort SORT_NAME_EMAIL = Sort.by(Sort.Direction.ASC, "name", "email");
 
-    private final CrudUserRepository crudRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaUserRepository(CrudUserRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    @Autowired
+    public DataJpaUserRepository(CrudUserRepository crudUserRepository) {
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public User save(User user) {
-        return crudRepository.save(user);
+        return crudUserRepository.save(user);
     }
 
     @Override
     public boolean delete(int id) {
-        return crudRepository.delete(id) != 0;
+        return crudUserRepository.delete(id) != 0;
     }
 
     @Override
     public User get(int id) {
-        return crudRepository.findById(id).orElse(null);
+        return crudUserRepository.findById(id).orElse(null);
     }
 
     @Override
     public User getByEmail(String email) {
-        return crudRepository.getByEmail(email);
+        return crudUserRepository.getByEmail(email);
     }
 
     @Override
     public List<User> getAll() {
-        return crudRepository.findAll(SORT_NAME_EMAIL);
+        return crudUserRepository.findAll(SORT_NAME_EMAIL);
     }
 }
